288970b5d817f8a955e041c9aa3493f4233f12af
Jacob Patton <jrobertpatton@gmail.com>
2016-06-14 12:27:45 -0500

    added signup state to app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,3 +2,11 @@
 angular.module('PhotographyStore', [ui.router]);
 
 // Configure different application states
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('signup', {
+        url: '/',
+        templateURL: /templates/_signupView.html
+      });
