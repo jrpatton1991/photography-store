af34995aae16c5200a69ea49b40811877f0ac05a
Jacob Patton <jrobertpatton@gmail.com>
2016-06-13 23:57:52 -0500

    sessions installed, required, and used in middleware

---
+++ models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var userSchema = new Schema({
+  username: {type: String},
+  password: {type: String},
+  email: {type: String}
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
