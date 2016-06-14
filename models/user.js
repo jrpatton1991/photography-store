35d3d00cf0fa96b2a36f2dc0d93a3a98562511aa
Jacob Patton <jrobertpatton@gmail.com>
2016-06-14 15:46:32 -0500

    login route functioning

--- models/user.js
+++ models/user.js
@@ -2,14 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
+
 var userSchema = new Schema({
   username: {type: String},
-  password: {type: String},
-  email: {type: String}
+  password: {type: String}
 });
 
 userSchema.plugin(passportLocalMongoose);
-
 var User = mongoose.model('User', userSchema);
+
 
 module.exports = mongoose.model('User', User);
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,11 +3,11 @@ var app=angular.module('PhotographyStore', ['ui.router']);
 
 // Configure different application states
 app.config(function($stateProvider, $urlRouterProvider) {
-    // $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/');
 
     $stateProvider
       .state('login', {
         url: '/login',
-        templateURL: "/templates/_loginView.html"
+        templateUrl: "/templates/_loginView.html"
       });
 });
