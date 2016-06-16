5e5ee762d632c65f93fb8c85b8f38e6b1b940d36
Jacob Patton <jrobertpatton@gmail.com>
2016-06-16 09:51:55 -0500

    photo json data rendering

---
+++ routes/photos.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var Student = require('./models/photo');
+
+// GET /photos
+router.get('/photos', function(req, res, next) {
+  Photo.find({}, function(err, photos) {
+    if (err) {
+      res.status(500).send();
+    } else {
+      res.json(photos);
+    }
+  });
+});
+
+// POST /students
+router.post('/photos', function(req, res, next) {
+  var photo = new Photo(req.body);
+  photo.save(function(err) {
+    if (err) {
+    res.status(500).send();
+    } else {
+    res.json(photos);
+    }
+  });
+});
+
+
+module.exports = router;
+
+
+// router.use('/photos', function(req, res, next) {
+//   Student.findOne({ 'largeUrl': req.params.largeUrl }, function(err, photo) {
+//     if (err) {
+//       res.status(500).send();
+//     } else {
+//       if (photo) {
+//         res.photo = photo;
+//         next();
+//       } else {
+//         res.status(404).send();
+//       }
+//     }
+//   });
+// });
